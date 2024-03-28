af83b8719ed97022be1118639139f8ad8cf37109
anthony <anthony.mackinnon@cryppro.com>
2024-03-28 08:10:55 +0000

    add solutions

--- quiz1/src/main.rs
+++ quiz1/src/main.rs
@@ -2,15 +2,21 @@
 // - Variables
 // - Functions
 // - If
-//
+
 // Mary is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
-// Write a function that calculates the price of an order of apples given the
-// quantity bought.
+// Write a function that calculates the price of an order of apples given
+// the quantity bought. No hints this time!
 
 // Put your function here!
-// fn calculate_price_of_apples {
+const fn calculate_price_of_apples(apples: u8) -> u8 {
+    if apples > 40 {
+        apples
+    } else {
+        apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
--- quiz2/src/main.rs
+++ quiz2/src/main.rs
@@ -4,10 +4,10 @@
 // - Move semantics
 // - Modules
 // - Enums
-//
-// Let's build a little machine in the form of a function. As input, we're going
-// to give a list of strings and commands. These commands determine what action
-// is going to be applied to the string. It can either be:
+
+// Let's build a little machine in the form of a function.
+// As input, we're going to give a list of strings and commands. These commands
+// determine what action is going to be applied to the string. It can either be:
 // - Uppercase the string
 // - Trim the string
 // - Append "bar" to the string a specified amount of times
@@ -15,6 +15,7 @@
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
+
 
 pub enum Command {
     Uppercase,
@@ -26,29 +27,42 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-// TODO: Complete the output declaration!
-let mut output: ??? = vec![];
-for (string, command) in input.iter() {
-// TODO: Complete the function body. You can do it!
-}
-output
-}
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
+        // TODO: Complete the output declaration!
+
+        // let mut output: ??? = vec![];
+        let mut output = Vec::with_capacity(input.len());
+        // for (string, command) in input.iter() {
+        for (string, command) in input {
+            // TODO: Complete the function body. You can do it!
+            output.push(match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_owned(),
+                // Command::Append(n) => format!("{string}{}", "bar".repeat(n)),
+                Command::Append(n) => string.to_owned() + &"bar".repeat(n),
+            });
+        }
+        output
+    }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
     fn it_works() {
         let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
+            // ("hello".into(), Command::Uppercase),
+            // (" all roads lead to rome! ".into(), Command::Trim),
+            // ("foo".into(), Command::Append(1)),
+            // ("bar".into(), Command::Append(5)),
+            ("hello", Command::Uppercase),
+            (" all roads lead to rome! ", Command::Trim),
+            ("foo", Command::Append(1)),
+            ("bar", Command::Append(5)),
         ]);
         assert_eq!(output[0], "HELLO");
         assert_eq!(output[1], "all roads lead to rome!");
